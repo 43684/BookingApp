05756cf0bc53b4bc4296623e70770f81217cce36
jhaard <jorgen.hard@gmail.com>
2024-05-27 14:59:48 +0200

    EmailVerificationView with functionality added.

--- BookingApp/Views/EmailVerificationView.swift
+++ BookingApp/Views/EmailVerificationView.swift
@@ -11,32 +11,41 @@ import FirebaseAuth
 struct EmailVerificationView: View {
     @State private var statusText: String?
     @State var nextView: Bool = false
-    //    @State private var email: String = ""
-    //    @State private var password: String = "temporary1234"
     
     @ObservedObject var viewModel = EmailVerificationViewModel()
     
     var body: some View {
-        NavigationStack {
+        NavigationView {
             VStack {
+                Image("Icon")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                
+                Spacer()
+                
+                Text("Dear Customer,\nYour appointment has been successfully confirmed. Please tap the button below to receive the verification email in your inbox.")
+                    .foregroundColor(Color(hex: "#D3BD9C"))
+                    .font(.title2)
+                    .bold()
+                    .padding(20)
                 
                 Spacer()
                 
                 Button(action: {
                     if viewModel.isMailVerified {
+                        viewModel.deleteUser()
                         nextView = true
                     }
                 }) {
-                    Text("Finalize Booking")
+                    Text("FINALIZE BOOKING")
                         .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(8)
+                        .font(.title3)
+                        .background(
+                            LinearGradient(gradient: Gradient(colors: [Color(hex: "#D3BD9C"), Color(hex: "#6D6355")]), startPoint: .leading, endPoint: .trailing))
+                        .foregroundColor(Color(hex: "#131D54"))
+                        .cornerRadius(15)
+                        .bold()
                 }
-                .navigationDestination(isPresented: $nextView, destination: {
-                    BookingCompleteView()
-                })
-                
                 .padding()
                 
                 Text(statusText ?? "Waiting for verification...")
@@ -44,6 +53,11 @@ struct EmailVerificationView: View {
                     .font(.title2)
                 
             }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
+            .navigationDestination(isPresented: $nextView, destination: {
+                BookingCompleteView()
+            })
         }
         
         .onAppear {
@@ -52,9 +66,11 @@ struct EmailVerificationView: View {
         .onChange(of: viewModel.isMailVerified) {
             if viewModel.isMailVerified == true {
                 viewModel.stopVerificationTimer()
-                viewModel.deleteUser()
                 statusText = "Verification OK!\nClick to finilize booking!"
             }
+        }
+        .onDisappear {
+            viewModel.stopVerificationTimer()
         }
     }
     
