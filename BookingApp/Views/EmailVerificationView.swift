69e42ba024e298e7828bd65dd69b5315dfa2a267
jhaard <jorgen.hard@gmail.com>
2024-05-28 08:46:47 +0200

    Correct spelling added.

--- BookingApp/Views/EmailVerificationView.swift
+++ BookingApp/Views/EmailVerificationView.swift
@@ -23,7 +23,7 @@ struct EmailVerificationView: View {
                 
                 Spacer()
                 
-                Text("Dear Customer,\nYour appointment has been successfully confirmed. Please check your inbox and verify your email-adress to proceed.")
+                Text("Dear Customer,\nYour appointment has been successfully confirmed. Please check your inbox and verify your email-address to proceed.")
                     .foregroundColor(Color(hex: "#D3BD9C"))
                     .font(.title2)
                     .bold()
